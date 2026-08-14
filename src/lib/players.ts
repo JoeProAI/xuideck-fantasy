c1ad3577c978326be371a0bfc4e0ab5832cfa292
JoeProAI <Joe@Joepro.ai>
2026-08-14 08:47:53 -0400

    Add real X player pool and generated route tree

---
+++ src/lib/players.ts
@@ -0,0 +1,192 @@
+import type { Player } from "./types";
+
+export const PLAYERS: Player[] = [
+  {
+    handle: "elonmusk",
+    name: "Elon Musk",
+    bio: "X / xAI / Tesla / SpaceX",
+    followers: 241_291_291,
+    verified: true,
+    lane: "Founder",
+    pfp: "https://pbs.twimg.com/profile_images/2053244804520427520/m8mdWZCG.jpg",
+    weeks: {
+      32: { posts: 41, likes: 920_000, impressions: 48_200_000, replies: 41_000, quotes: 9_400, bookmarks: 18_200, homeRuns: 6 },
+      33: { posts: 28, likes: 214_000, impressions: 9_800_000, replies: 12_400, quotes: 3_100, bookmarks: 6_800, homeRuns: 2 },
+    },
+  },
+  {
+    handle: "grok",
+    name: "Grok",
+    bio: "@grok it",
+    followers: 8_964_564,
+    verified: true,
+    lane: "Platform",
+    pfp: "https://pbs.twimg.com/profile_images/2057885623374290944/lmj0pawP.jpg",
+    weeks: {
+      32: { posts: 64, likes: 38_400, impressions: 6_100_000, replies: 6_200, quotes: 410, bookmarks: 1_140, homeRuns: 1 },
+      33: { posts: 86, likes: 61_200, impressions: 8_400_000, replies: 9_800, quotes: 720, bookmarks: 2_050, homeRuns: 3 },
+    },
+  },
+  {
+    handle: "cb_doge",
+    name: "DogeDesigner",
+    bio: "Design at X",
+    followers: 1_895_815,
+    verified: true,
+    lane: "Creator",
+    pfp: "https://pbs.twimg.com/profile_images/1498070100393754625/C2V-fbll.jpg",
+    weeks: {
+      32: { posts: 22, likes: 84_000, impressions: 5_600_000, replies: 3_400, quotes: 890, bookmarks: 2_200, homeRuns: 2 },
+      33: { posts: 18, likes: 71_500, impressions: 4_200_000, replies: 2_900, quotes: 640, bookmarks: 1_760, homeRuns: 1 },
+    },
+  },
+  {
+    handle: "nikitabier",
+    name: "Nikita Bier",
+    bio: "ex-head of product @X",
+    followers: 1_286_666,
+    verified: true,
+    lane: "Product",
+    pfp: "https://pbs.twimg.com/profile_images/1755448801957945344/Fh2HNw5Y.jpg",
+    weeks: {
+      32: { posts: 14, likes: 42_000, impressions: 2_800_000, replies: 2_100, quotes: 380, bookmarks: 1_050, homeRuns: 1 },
+      33: { posts: 11, likes: 28_400, impressions: 1_640_000, replies: 1_420, quotes: 210, bookmarks: 740, homeRuns: 0 },
+    },
+  },
+  {
+    handle: "Engineering",
+    name: "Engineering",
+    bio: "updates from our engineering team",
+    followers: 1_311_232,
+    verified: true,
+    lane: "Platform",
+    pfp: "https://pbs.twimg.com/profile_images/1683502255574593536/anU7i4uA.jpg",
+    weeks: {
+      32: { posts: 6, likes: 18_200, impressions: 1_420_000, replies: 640, quotes: 120, bookmarks: 880, homeRuns: 0 },
+      33: { posts: 4, likes: 22_800, impressions: 1_980_000, replies: 910, quotes: 180, bookmarks: 1_140, homeRuns: 1 },
+    },
+  },
+  {
+    handle: "AlexFinn",
+    name: "Alex Finn",
+    bio: "Founder, Henry Intelligent Machines",
+    followers: 466_904,
+    verified: true,
+    lane: "Creator",
+    pfp: "https://pbs.twimg.com/profile_images/1745232634278461440/7gQcr_R_.jpg",
+    weeks: {
+      32: { posts: 19, likes: 14_800, impressions: 980_000, replies: 1_120, quotes: 240, bookmarks: 610, homeRuns: 0 },
+      33: { posts: 21, likes: 31_400, impressions: 2_240_000, replies: 2_680, quotes: 510, bookmarks: 1_420, homeRuns: 2 },
+    },
+  },
+  {
+    handle: "aakashgupta",
+    name: "Aakash Gupta",
+    bio: "Product writing and interviews",
+    followers: 312_749,
+    verified: true,
+    lane: "Writer",
+    pfp: "https://pbs.twimg.com/profile_images/2021355466216062976/8MDXp7vR.jpg",
+    weeks: {
+      32: { posts: 12, likes: 9_400, impressions: 640_000, replies: 780, quotes: 160, bookmarks: 520, homeRuns: 0 },
+      33: { posts: 9, likes: 24_600, impressions: 1_860_000, replies: 1_940, quotes: 430, bookmarks: 1_680, homeRuns: 1 },
+    },
+  },
+  {
+    handle: "tetsuoai",
+    name: "tetsuo",
+    bio: "C and Assembly. Grok.",
+    followers: 236_580,
+    verified: true,
+    lane: "Engineer",
+    pfp: "https://pbs.twimg.com/profile_images/2035289570598924288/kAOCyKpV.jpg",
+    weeks: {
+      32: { posts: 28, likes: 8_200, impressions: 520_000, replies: 940, quotes: 90, bookmarks: 310, homeRuns: 0 },
+      33: { posts: 24, likes: 11_400, impressions: 740_000, replies: 1_280, quotes: 140, bookmarks: 460, homeRuns: 0 },
+    },
+  },
+  {
+    handle: "kcoleman",
+    name: "Keith Coleman",
+    bio: "VP Product @X",
+    followers: 39_678,
+    verified: true,
+    lane: "Product",
+    pfp: "https://pbs.twimg.com/profile_images/1093367487566012416/9Wc1J-Pk.jpg",
+    weeks: {
+      32: { posts: 8, likes: 3_100, impressions: 210_000, replies: 420, quotes: 60, bookmarks: 190, homeRuns: 0 },
+      33: { posts: 7, likes: 6_800, impressions: 480_000, replies: 710, quotes: 95, bookmarks: 340, homeRuns: 0 },
+    },
+  },
+  {
+    handle: "futureoffantasy",
+    name: "Josh Culp",
+    bio: "Fantasy Golf. Formerly Rotoworld / NBC Sports",
+    followers: 12_611,
+    verified: true,
+    lane: "Sports",
+    pfp: "https://pbs.twimg.com/profile_images/1492956262262263815/d6zuoa9Z.jpg",
+    weeks: {
+      32: { posts: 34, likes: 2_840, impressions: 186_000, replies: 310, quotes: 40, bookmarks: 88, homeRuns: 0 },
+      33: { posts: 29, likes: 2_210, impressions: 142_000, replies: 260, quotes: 28, bookmarks: 64, homeRuns: 0 },
+    },
+  },
+  {
+    handle: "plbiojout",
+    name: "Pierre-Louis Biojout",
+    bio: "building Nanocorp · YC",
+    followers: 7_502,
+    verified: true,
+    lane: "Founder",
+    pfp: "https://pbs.twimg.com/profile_images/1663600679934361621/mlJ779GR.jpg",
+    weeks: {
+      32: { posts: 11, likes: 1_640, impressions: 98_000, replies: 240, quotes: 36, bookmarks: 120, homeRuns: 0 },
+      33: { posts: 8, likes: 4_920, impressions: 410_000, replies: 680, quotes: 110, bookmarks: 390, homeRuns: 1 },
+    },
+  },
+  {
+    handle: "XOpenSource",
+    name: "X Open Source",
+    bio: "The For You ranking code, in public",
+    followers: 6_416,
+    verified: true,
+    lane: "Platform",
+    pfp: "https://pbs.twimg.com/profile_images/2086823682895179776/inMppXhg.jpg",
+    weeks: {
+      32: { posts: 3, likes: 420, impressions: 31_000, replies: 48, quotes: 6, bookmarks: 40, homeRuns: 0 },
+      33: { posts: 6, likes: 18_600, impressions: 2_140_000, replies: 2_240, quotes: 610, bookmarks: 1_880, homeRuns: 3 },
+    },
+  },
+  {
+    handle: "orbitant",
+    name: "orbitant",
+    bio: "AI + Web3 observer",
+    followers: 3_524,
+    verified: true,
+    lane: "Analyst",
+    pfp: "https://pbs.twimg.com/profile_images/1809492145587580929/sTOA39Ab.jpg",
+    weeks: {
+      32: { posts: 16, likes: 890, impressions: 54_000, replies: 160, quotes: 18, bookmarks: 52, homeRuns: 0 },
+      33: { posts: 14, likes: 2_740, impressions: 228_000, replies: 410, quotes: 55, bookmarks: 180, homeRuns: 0 },
+    },
+  },
+  {
+    handle: "JoePro",
+    name: "JoePro",
+    bio: "CS/Math. Teaches AI to humans.",
+    followers: 981,
+    verified: true,
+    lane: "Writer",
+    pfp: "https://pbs.twimg.com/profile_images/2069590429083766784/pjgG-Bbu.jpg",
+    weeks: {
+      32: { posts: 9, likes: 186, impressions: 18_400, replies: 42, quotes: 6, bookmarks: 21, homeRuns: 0 },
+      33: { posts: 12, likes: 940, impressions: 186_000, replies: 210, quotes: 34, bookmarks: 128, homeRuns: 1 },
+    },
+  },
+];
+
+export function playerByHandle(handle: string) {
+  return PLAYERS.find((p) => p.handle.toLowerCase() === handle.toLowerCase());
+}
+
+export const DESK_SEVEN = ["cb_doge", "nikitabier", "Engineering", "tetsuoai", "orbitant"];
